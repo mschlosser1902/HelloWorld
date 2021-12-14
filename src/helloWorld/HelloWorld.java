91570024cfb68444627466753971f4ddca81146b
m.schlosser <michael.schlosser@fls-wiesbaden.de>
2021-12-14 13:08:04 +0100

    text geändert as pull req

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloWorld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hallo Clone");
+        System.out.println("pull req");
     }
 
 
